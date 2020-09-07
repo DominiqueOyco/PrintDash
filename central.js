2a072f6b3c33e9b4bebc1c77fc0c4d96643bf9fd
DominiqueOyco <dominickoyco@gmail.com>
2020-09-07 11:04:03 -0700

    new file added

--- central.js
+++ central.js
@@ -47,20 +47,4 @@ window._wpemojiSettings = {"baseUrl":
 }
 (window,document,window._wpemojiSettings);
 
-//this section is dedicated to the schedule-home section of the homepage
-var today = new Date();
-var now = today.getDay();
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-document.open();
-document.getElementById('schedule-contents').innerHTML += "<strong>" + days[now];
-today = ' ' + mm + '/' + dd + '/' + yyyy;
-document.getElementById('schedule-contents').innerHTML += "<strong>" + today + "<br> <br>";
-
-document.getElementById('schedule-contents').innerHTML += "<br>" + "<a href='airings.html'><strong>SEE THE ENTIRE SCHEDULE!</strong></a>";
-document.close();
-
 
